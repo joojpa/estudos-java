0a673d1ba585a06f076d793afc97d853d64b47ce
JooJ <joaopedroacostaalvarez@gmail.com>
2026-01-30 20:05:39 -0300

    Exercise 6 five%value

---
+++ src/logicadeprogramacao/exercicios/Exercise6.java
@@ -0,0 +1,13 @@
+package logicadeprogramacao.exercicios;
+
+import java.util.Scanner;
+
+public class Exercise6 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        double value = sc.nextDouble();
+
+        System.out.println(value + value * 0.05);
+    }
+}
